026a253c20f274f9c2afb329db93216d86ac2191
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-12 23:13:19 +0400

    I  Creates a Scanner object ,Imports the scanner class and closes the Scanner

--- Java Task 12/src/Task420/Main.java
+++ Java Task 12/src/Task420/Main.java
@@ -1,4 +1,15 @@
 package Task420;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+
+    }
 }
