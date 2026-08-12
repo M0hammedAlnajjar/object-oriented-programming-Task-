c9df4dbaa896d41b0a91a5baf93d9a972a090c04
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-12 21:33:14 +0400

    I Asks the user to enter the plate number and reads the plate number from the user

--- Java Task 12/src/Task419/Main.java
+++ Java Task 12/src/Task419/Main.java
@@ -13,6 +13,8 @@ public class Main {
         System.out.println("Enter car colour:");
         car.colour = sc.nextLine();
 
+        System.out.println("Enter plate:");
+        car.plate = sc.nextLine();
 
         sc.close();
 
