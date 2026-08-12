ee1c28a8df4c4a312dd30bf8d763df0082832f32
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-12 23:36:10 +0400

    I Calls the eat ,sleep and make sound method

--- Java Task 12/src/Task422/Main.java
+++ Java Task 12/src/Task422/Main.java
@@ -3,5 +3,9 @@ package Task422;
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
+
+        dog.eat();
+        dog.sleep();
+        dog.makeSound();
     }
     }
